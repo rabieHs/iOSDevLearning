c8d2d404f11fa8371de14a2a97cf80a82124b3c2
rabieHs <rabiehoussaini@gmail.com>
2025-02-02 13:32:29 +0100

    Initial Commit

---
+++ iOSDevLearning/iOSDevLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDevLearningApp.swift
+//  iOSDevLearning
+//
+//  Created by Apple on 02/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOSDevLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
